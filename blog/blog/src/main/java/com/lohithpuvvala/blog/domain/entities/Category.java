d67622fbbf7c0e7e5f2a991d0f86bf7c88925ca5
lohithpuvvala <lohithpuvvala@gmail.com>
2026-04-18 14:49:31 +0530

    Add Category Entity

---
+++ blog/blog/src/main/java/com/lohithpuvvala/blog/domain/entities/Category.java
@@ -0,0 +1,34 @@
+package com.lohithpuvvala.blog.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
