edc535ea7fa7bfa85c05ebaea17c151e9af6892e
lohithpuvvala <lohithpuvvala@gmail.com>
2026-04-18 16:47:15 +0530

    Create repositories/CategoryRepository.java interface

--- blog/blog/src/main/java/com/lohithpuvvala/blog/repositories/CategoryRepository.java
+++ blog/blog/src/main/java/com/lohithpuvvala/blog/repositories/CategoryRepository.java
@@ -1,4 +1,10 @@
 package com.lohithpuvvala.blog.repositories;
 
-public interface CategoryRepository {
-}
+import com.lohithpuvvala.blog.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> { }
